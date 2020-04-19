848868c360eb0f48f45c3d28ac8a1e85911dd262
gregorseifer <segr1011@hs-karlsruhe.de>
2020-04-19 19:22:11 +0200

    add exceptions for null args

--- server/src/service/SupplierService.cs
+++ server/src/service/SupplierService.cs
@@ -9,7 +9,8 @@ namespace Suppliers
     {
         List<Supplier> supplierList;
         List<Product> productList;
-        public SupplierService() {
+        public SupplierService()
+        {
             supplierList = TestData.createTestSupplierList();
             productList = TestData.createTestProductList();
         }
@@ -19,22 +20,30 @@ namespace Suppliers
             List<string> supplierIds = productList.FindAll(product => product.SupplierFk != null)
                 .Select(product => product.SupplierFk)
                 .ToList();
-            return supplierList.FindAll( supplier => supplierIds.Contains(supplier.Id) );
+            return supplierList.FindAll(supplier => supplierIds.Contains(supplier.Id));
         }
-        public  Supplier findPreferredSupplier(Product product) {
-            var p_fk = productList.Find( (Product p) => product.Id == p.Id ).SupplierFk;
-            return supplierList.Find( supplier => p_fk == supplier.Id );
+        public  Supplier findPreferredSupplier(Product product)
+        {
+            if (product == null) throw new RpcException(new Status(
+                StatusCode.InvalidArgument, "product darf nicht null sein"));
+            var p_fk = productList.Find((Product p) => product.Id == p.Id).SupplierFk;
+            return supplierList.Find(supplier => p_fk == supplier.Id);
         }
-        public void setPreferredSupplierForProduct(Supplier supplier, Product product) {
-            var pId = (product??throw new ArgumentNullException()).Id;
-            var sId = (supplier??throw new ArgumentNullException()).Id;
+        public void setPreferredSupplierForProduct(Supplier supplier, Product product)
+        {
+            var pId = (product??throw new RpcException(new Status(
+                StatusCode.InvalidArgument, "supplier darf nicht null sein"
+            ))).Id;
+            var sId = (supplier??throw new RpcException(new Status(
+                StatusCode.InvalidArgument, "product darf nicht null sein"
+            ))).Id;
             if (productList.FindAll((Product p) => p.Id == pId).Count == 0) {
                 throw new UnknownProductException();
             }
             if (supplierList.FindAll((Supplier s) => s.Id == sId).Count == 0) {
                 throw new UnknownSupplierException();
             }
-            productList.Find( (Product p) => p.Id == pId ).SupplierFk = sId;
+            productList.Find((Product p) => p.Id == pId).SupplierFk = sId;
         }
     }
 }
